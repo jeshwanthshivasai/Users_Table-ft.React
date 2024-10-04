1f1b006e5a2104b9f3d19be93e9423169ca240b7
jeshwanthshivasai <havingthesensesofaspider@gmail.com>
2024-10-04 23:05:29 +0530

    mmm

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ function App() {
   const [users, setUsers] = useState([]);
   const [headings, setHeadings] = useState([]);
 
-  // Fetch data from API
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
